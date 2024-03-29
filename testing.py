67f4c8ee687a8929763b5234efabe249d4f88035
talberdichevsky <berdichevsky.t@northeastern.edu>
2024-03-29 19:24:01 -0400

    Some utility functions for preprocessing, train/validate/test separation, and pre-evaluation Also deleted outdated funcs I wrote before in the test.py file

--- testing.py
+++ testing.py
@@ -3,7 +3,7 @@ import pickle
 with open('dataset.pkl', 'rb') as file:
     dataset:dict = pickle.load(file)
 
-# List some attributes about peices in the dataset
+# List some attributes about pieces in the dataset
 """
 for piece_name in list(dataset.keys())[:2]:
     piece = dataset[piece_name]
@@ -18,47 +18,7 @@ for piece_name in list(dataset.keys())[:2]:
     print(f'nmat={nmat}')
     print(f'root={root}')
 """
-# Attempt to find overlapping notes!
-#create a new list with only pieces that don't have overlaps in them
-no_overlaps = list()
-for piece_name in list(dataset.keys()):
-    piece = dataset[piece_name]
-    nmat = piece['nmat']
-    root = piece['root']
-    mode = piece['mode']
-    tonic = piece['tonic']
-    found_overlap = False  # Flag to indicate overlap
-    for i in range(1, len(nmat)):
-        prev = nmat[i-1]
-        curr = nmat[i]
-        same_time_frame = prev[0] == curr[0] and prev[1] == curr[1]  # True if they're at the same time
-        subsequent = prev[1] <= curr[0]  # True if they don't overlap
-        if not (subsequent or same_time_frame):
-            found_overlap = True
-            break
-    if not found_overlap:
-        no_overlaps.append((nmat, root, mode, tonic))
 
-#function for later, takes out the last note/group of notes if there are several at the same time
-#for the model to predict it given the original sequence
-#this function assumes no note overlaps
-#returns a tuple (array of last note group, array of notes beforehand)
-def separate_last_note_group(nmat):
-    x = len(nmat)-1 #index for last note
-    last_group =[]
-    for i in range(x, -1, -1):
-        curr = nmat[i]
-        if curr[0] != nmat[x][0]:
-            break
-        last_group.append(curr)
-    last_group.reverse()
-    return last_group, nmat[:-(len(last_group))]
 
-#testing
-"""
-piece = no_overlaps[1100][0]
-print(f'original: {piece}')
-last_group, all_but_last = separate_last_note_group(piece)
-print(f'last note group: {last_group}, all but last: {all_but_last}')
-"""
+
 
---
+++ utils.py
@@ -0,0 +1,74 @@
+import pickle
+import statistics
+import librosa as lbs
+from tqdm import tqdm
+
+with open('dataset.pkl', 'rb') as file:
+    dataset:dict = pickle.load(file)
+
+midi_to_note_dict = {}
+
+def midi_to_note(midi_number):
+    if midi_number not in midi_to_note_dict:
+        midi_to_note_dict[midi_number] = lbs.midi_to_note(midi_number, unicode=False)
+    return midi_to_note_dict[midi_number]
+
+
+"""
+Returns an array of 
+    dictionaries for every piece {time, (mean amplitude, [notes at that point])}
+"""
+def preprocess():
+    all_pieces = list()
+    for piece_name in tqdm(list(dataset.keys())):
+        notes_per_time = {}
+        piece = dataset[piece_name]
+        nmat = piece['nmat']
+        piece_length = nmat[len(nmat)-1][1] #last time of last note playing
+        for i in range(0, piece_length+1): #for each second, what notes are playing?
+            amplitudes = []
+            notes = []
+            for note in nmat:
+                if i>=note[0] and i<=note[1]: #it's in the range when the note plays
+                    amplitudes.append(note[3])
+                    notes.append(midi_to_note(note[2]))
+                elif note[0]>i: #once the start time is after i, break
+                    break
+            mean_amp = 0 if amplitudes==[] else statistics.mean(amplitudes)
+            notes_per_time[i] = (mean_amp, notes)
+        all_pieces.append(notes_per_time)
+    return all_pieces
+
+pieces_beats = preprocess()
+
+"""
+For validation/testing, separates a given piece into x last beats and len-x previous notes/chords.
+Takes in a dictionary {beat, (mean amplitude, [notes at that point])}
+and x=number of beats to separate form the end
+
+Returns tuple of two dicts: (dict_with_last_x_items, dict_with_the_rest)
+"""
+def separate_last_note_group(piece, x):
+    if x == 0 or x < 0:
+        return {}, piece
+    items = list(piece.items())
+    dict_with_last_x_items = dict(items[-x:])
+    dict_with_the_rest = dict(items[:-x])
+    return dict_with_last_x_items, dict_with_the_rest
+
+"""
+Separate preprocessed data into training(80%), validation(10%), and test(10%) sets.
+Returns a tuple (training, validation, test)
+"""
+def separate_for_training(pieces):
+    train_end_idx = int(0.8 * len(pieces))
+    validate_end_idx = int(0.9 * len(pieces))
+
+    train = pieces[:train_end_idx]
+    validate = pieces[train_end_idx:validate_end_idx]
+    test = pieces[validate_end_idx:]
+
+    return train, validate, test
+
+train, validate, test = separate_for_training(pieces_beats)
+
