ae508f6736f28de680459f2dc5d4b16ac2b22e91
Gianna Mattessich <giannaa888@gmail.com>
2024-04-07 12:33:41 -0400

    chroma bug fixes

--- chroma.py
+++ chroma.py
@@ -8,11 +8,11 @@ import re
 import seaborn as sns
 from scipy.ndimage import gaussian_filter
 
-# with open(r"dataset.pkl", 'rb') as data:
-#     midi_data = pickle.load(data)
+with open(r"dataset.pkl", 'rb') as data:
+    midi_data = pickle.load(data)
 
 notes =  ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-
+FULL_CHORD_LIST = [note + suffix for note in notes for suffix in ['', 'm', 'dim']]
 
 '''Takes in name of sample (str) and outputs the labeled progression of either length 32 or length 64
     Parameters:
@@ -75,97 +75,37 @@ def get_note_map(midi_seq, midi_data):
     return note_map
 
 '''
-Function to rotate the list of notes so that the first element becomes the root and the following elements are the consecutive chords rotated for that scale
-
-Parameters: 
-    original notes C through B (list)
-    tonic from midi sequence data (int)
-
-Output:
-    rotated list of note strings
+Function that parses the label to get prog info and gets the true relabeled progression for a given piece
+    Params:
+        -sample: sample name(str)
+        -midi_data: midi dataset(dict)
+    Outputs:
+        -pandas series with the correctly labeled progression for sequence 
 '''
-# change the indices of the notes so that the key will be the first chord 
-def rotate_list(notes, tonic):
-    try:
-        # get root note of first chord in prog
-        key_root = notes[tonic]
-        # get the curr index of the root note in notes list 
-        index = notes.index(key_root)
-    except ValueError:
-        print(f"{key_root} not found in the list.")
-        return notes
-    # use splicing and concatonation to rotate the notes list so that there are still 12 elements but the first element and preceding elements are flipped for the given key 
-    rotated_lst = notes[index:] + notes[:index]
-    return rotated_lst
-
-'''
-Function that takes in string of sample name with a major mode
-and outputs a list of the chords in that key -> use for label realignment
-
-Parameters:
-    midi sample name (str)
-    
-Output:
-    chords in the key of the provided sequence name
-'''
-def get_maj_scale(notes, midi_seq, midi_data):
-    seq = midi_data[midi_seq]
-    key_chords = []
-    # get key info
-    tonic = seq['tonic']
-    notes_rotated = rotate_list(notes, tonic)
-    '''
-    1) tonic, root (major)
-    2) 2 semitones up (minor)
-    3) 2 semitones up (minor)
-    4) 1 semitone up (major)
-    5) 2 semitones up (major)
-    6) 2 semitones up (minor)
-    7) 2 semitones up (diminished)
-    '''
-    key_chords.append(notes_rotated[0])
-    key_chords.append(notes_rotated[2] + 'm')
-    key_chords.append(notes_rotated[4] + 'm')
-    key_chords.append(notes_rotated[5])
-    key_chords.append(notes_rotated[7])
-    key_chords.append(notes_rotated[9] + 'm')
-    key_chords.append(notes_rotated[11] + 'dim')
-    return key_chords
-
-'''
-Function that takes in string of sample name with a minor mode
-and outputs a list of the chords in that key 
-
-Parameters:
-    midi sample name (str)
-    
-Output:
-    chords in the key of the provided sequence name
-'''
-def get_minor_scale(notes, midi_seq, midi_data):
-    seq = midi_data[midi_seq]
-    key_chords = []
-    # get key info
-    tonic = seq['tonic']
-    notes_rotated = rotate_list(notes, tonic)
-    '''
-    1) tonic, root (minor)
-    2) 2 semitones up (diminished)
-    3) 1 semitone up (major)
-    4) 2 semitones up (minor)
-    5) 2 semitones up (minor)
-    6) 1 semitones up (major)
-    7) 2 semitones up (major)
-    '''
-    key_chords.append(notes_rotated[0] + 'm')
-    key_chords.append(notes_rotated[2] + 'dim')
-    key_chords.append(notes_rotated[3])
-    key_chords.append(notes_rotated[5] + 'm')
-    key_chords.append(notes_rotated[7] + 'm')
-    key_chords.append(notes_rotated[8])
-    key_chords.append(notes_rotated[10])
-    return key_chords
-
+def get_chord_labels(sample, midi_data):
+    progression = pd.Series(get_progression(sample, midi_data))
+    roots_in_seq = list(set(progression))
+    new_prog = progression.copy()
+    # split samples by '_' and '-' characters 
+    for string in sample.split('_'):
+        if '-' in string:
+            prog = string.split('-')
+            prog = (' ').join(prog).split(' ')
+            # filter out chords not in prog list 
+            chords = [chord for chord in prog if chord in FULL_CHORD_LIST]
+            if len(chords) > 0:
+                labels_to_string = (' ').join(chords)
+                for chord in roots_in_seq:
+                    if chord not in chords:
+                        # define regex pattern to get an instance of the chord 
+                        pattern = r'\b\w*{}\w*\b'.format(re.escape(chord))
+                        # replace the root labels with the true 
+                        matches = re.findall(pattern, labels_to_string, flags=re.IGNORECASE)
+                        if len(matches) == 0:
+                            continue
+                        true_chord = matches[0]
+                        new_prog = new_prog.replace(to_replace=chord, value=true_chord)
+    return new_prog 
 
 '''
 Takes in name of sample (str) and outputs chromagram data
@@ -176,7 +116,6 @@ Output:
     chromagram dataframe: dataframe with time as the index, note bins as columns with values being their amplitude, and a column at the end that 
     contains the labeled root of the chord at that time 
 '''
-
 def get_chromagram(midi_seq, midi_data):
     # get map of times and note amplitudes at each time
     note_data = get_note_map(midi_seq, midi_data)
@@ -187,31 +126,7 @@ def get_chromagram(midi_seq, midi_data):
     map = [list(note_data[time].values()) for time in range(duration)] 
     # flip rows and cols -> pitch class as rows and times as cols 
     chroma_map = pd.DataFrame(map, columns=notes)
-    seq = midi_data[midi_seq]
-    # check if sequence is in a minor or major scale
-    if seq['mode'] == 'm':
-        seq_scale = get_minor_scale(notes, midi_seq, midi_data)
-    else:
-        seq_scale = get_maj_scale(notes, midi_seq, midi_data)
-    # get current chords from chromagram
-    roots_in_seq = list(set(progression))
-    # join list of chords into a single string to parse for regex and replace in chroma labels
-    # each seq_scale is of length 7
-    scale_to_string = (' ').join(seq_scale)
-    # get labels for all timepoints 
-    chroma_map.insert(12, 'Chord Actual', value=progression)
-    # get chord list from the sequence 
-    # realign chord labels for minor and diminished chords 
-    for chord in roots_in_seq:
-        if chord not in seq_scale:
-            # define regex pattern to get an instance of the chord 
-            pattern = r'\b\w*{}\w*\b'.format(re.escape(chord))
-            # replace the root labels with the true 
-            matches = re.findall(pattern, scale_to_string, flags=re.IGNORECASE)
-            if len(matches) == 0: continue
-            true_chord = matches[0]
-            # realign the labels from sequence scale information
-            chroma_map['Chord Actual'] = chroma_map['Chord Actual'].replace(to_replace=chord, value=true_chord)
+    chroma_map['Chord Actual'] = get_chord_labels(midi_seq, midi_data)
     # filter out sequences that have nothing at end 
     chroma_map = chroma_map.loc[(chroma_map.loc[:, notes] != 0).any(axis=1)]
     return chroma_map
@@ -248,10 +163,5 @@ def get_chromagram_plot(midi_seq, midi_data):
     ax.set_xlabel('Time (s)')
     ax.set_ylabel('Note')
     ax.set_title(midi_seq)
-    # # apply filter to heatmap
-    # data_filter = gaussian_filter(chroma_map, sigma=0.5)  # Adjust sigma for more or less blur
-    # # overlay blurred data
-    # sns.heatmap(data_filter, cmap='viridis', alpha=0.5, cbar=False)
-    # make notes from C-Bb instead of Bb-C on plot
     plt.gca().invert_yaxis()
     plt.show()
