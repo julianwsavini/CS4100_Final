ed9d29bd61e75a958e8af1bcb873f92a2161ba7f
Gianna Mattessich <85815866+giannamattessich@users.noreply.github.com>
2024-04-09 11:39:33 -0400

    add voting and midi classify recognition

--- midi_classifier.py
+++ midi_classifier.py
@@ -1,5 +1,3 @@
-import joblib
-# import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import mido
 import numpy as np
@@ -8,9 +6,18 @@ import librosa
 import pickle 
 import warnings
 warnings.filterwarnings(action='ignore')
+import fluidsynth
 
+# Initialize FluidSynth
+synth = fluidsynth.Synth()
+synth.start()
+
+# Load a soundfont
+sfid = synth.sfload(r"Yamaha_MA2.sf2")
+synth.program_select(0, sfid, 0, 0)
+fluidsynth.init("Yamaha_MA2.sf2")
 # # Load the saved model from the file
-vote_classifier = pickle.load(open('dt.pkl', "rb"))
+vote_classifier = pickle.load(open('voting.pkl', "rb"))
 
 notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 active_notes = {}
@@ -24,14 +31,26 @@ def get_input_for_pred(active_notes):
     scaled_input.columns = notes
     return scaled_input
 
+
 with mido.open_input() as inport:
-    for msg in inport:
-        if msg.type == 'note_on':
-            if msg.velocity != 0:
-                active_notes[librosa.midi_to_note(msg.note, octave=False, unicode=False)] = msg.velocity
-                if len(active_notes) > 2:
-                    pred_input = get_input_for_pred(active_notes)
-                    prediction = vote_classifier.predict(pred_input)[0]
-                    print(f'Chord Prediction: {prediction}')
-            else:
-                active_notes = {}
+        try: 
+            for msg in inport:
+                if msg.type == 'note_on':
+                    if msg.velocity != 0:
+                        active_notes[librosa.midi_to_note(msg.note, octave=False, unicode=False)] = msg.velocity
+                        for note in active_notes:
+                            pass
+                            #fluidsynth.play_Note(Note(note), msg.velocity)
+                        if len(active_notes) > 2:
+                            pred_input = get_input_for_pred(active_notes)
+                            prediction = vote_classifier.predict(pred_input)[0]
+                            print(f'Chord Prediction: {prediction}')
+                    else:
+                        active_notes = {}  
+                        synth.noteoff(0, msg.note)
+        except KeyboardInterrupt:
+            pass
+        finally:
+            # Clean up before exiting
+            inport.close()
+            synth.delete()
---
+++ voting_classify_chord.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from chroma import *
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+import pickle
+import matplotlib.pyplot as plt
+import seaborn as sns
+import seaborn as sns 
+
+all_chromas = pd.concat([get_chromagram(sample, midi_data) for sample in list(midi_data.keys())])
+outputs = all_chromas['Chord Actual']
+inputs = all_chromas.drop(labels=['Chord Actual'], axis=1)
+X_train, X_test, y_train, y_test = train_test_split(inputs, outputs, test_size=0.2)
+
+# Step 2: Feature scaling
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Assuming X_train, X_test are your input data and y_train, y_test are the corresponding labels
+# Initialize and train the Decision Tree Classifier
+dt_classifier = DecisionTreeClassifier(random_state=65)
+dt_classifier.fit(X_train_scaled, y_train)
+
+# Initialize base classifiers
+knn_classifier = KNeighborsClassifier(n_neighbors=3)
+knn_classifier.fit(X_train_scaled, y_train)
+
+rf_classifier = RandomForestClassifier(n_estimators=100, random_state=65)
+rf_classifier.fit(X_train_scaled, y_train)
+
+# Create a Voting Classifier with the base classifiers
+voting_classifier = VotingClassifier(estimators=[('knn', knn_classifier), ('dt', dt_classifier), ('rf', rf_classifier)], voting='soft')
+voting_classifier.fit(X_train_scaled, y_train)
+
+pickle.dump(voting_classifier, open('voting.pkl', "wb"))
+
+# # Make predictions on the test set
+y_pred = voting_classifier.predict(X_test_scaled)
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy}')
+conf_matrix = confusion_matrix(y_test, y_pred)
+labels = list(np.unique(y_pred))
+plt.figure(figsize=(6, 4))
+sns.heatmap(conf_matrix, cmap='YlGnBu', fmt='d', cbar=False, xticklabels=labels, yticklabels=labels)
+plt.xlabel('Predicted labels')
+plt.ylabel('True labels')
+plt.title('Confusion Matrix')
+plt.show()
+# t_z = np.zeros(12)
+# t_z[11] = 80
+# t_z[2] = 90
+# t_z[7] = 127
+# test = pd.DataFrame(t_z).transpose()
+# test.columns = notes
+# new_scaled_test = scaler.transform(test)
+# print(voting_classifier.predict(new_scaled_test))
